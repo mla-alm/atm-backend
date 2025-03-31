ef7518becc15736860c134726cbf34581bdb7b63
mla-alm <mla@specialisterne.com>
2025-03-31 16:23:54 +0200

    Add value to UI

--- AtmBackend/Models/Responses/DenominationResponse.cs
+++ AtmBackend/Models/Responses/DenominationResponse.cs
@@ -2,8 +2,8 @@ using AtmBackend.Denominations;
 
 namespace AtmBackend.Models.Responses;
 
-public record DenominationResponse(ushort Amount, string DenominationName, PayBox PayBox)
+public record DenominationResponse(ushort Amount, string DenominationName, PayBox PayBox, ushort Value)
 {
     internal static DenominationResponse FromDenominationAmount(DenominationAmount denominationAmount) =>
-        new(denominationAmount.Amount, denominationAmount.Denomination.Name, denominationAmount.Denomination.PayBox);
+        new(denominationAmount.Amount, denominationAmount.Denomination.Name, denominationAmount.Denomination.PayBox, denominationAmount.Denomination.Value);
 }
